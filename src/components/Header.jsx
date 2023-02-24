53a48816feab4f8d50d95ca0a25bf2863dc4cadc
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2023-02-24 22:48:53 +0200

    Install tailwindCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ const navLinks = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
 
 const Header = () => {
   return (
-    <header>
+    <header className='flex justify-between items-center p-5'>
       <h1>React Vite Tutorial</h1>
       <nav>
-        <ul>
+        <ul className='flex gap-5'>
           {navLinks.map((link, index) => (
             <li key={index}>
               <a href={`/${link.toLowerCase()}`}>{link}</a>
